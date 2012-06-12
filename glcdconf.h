cb0cbb29e89bc84e1a56794177c9e8325bf4a32d
Tectu <joel@unormal.org>
2012-06-12 19:24:27 +0200

    added glcdconf.h file

---
+++ glcdconf.h
@@ -0,0 +1,14 @@
+#ifndef GLCDCONF_H
+#define GLCDCONF_H
+
+
+/***** LCD CONTROLLER *****/
+#define LCD_USE_SSD1289
+//#define LCD_USE_S6D1121
+
+
+/***** TOUCHPAD CONTROLLER *****/
+#define TOUCHPAD_USE_ADS7843
+
+
+#endif
