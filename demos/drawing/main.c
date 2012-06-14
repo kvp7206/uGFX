83d3135ab1683c29edc6e76ad34ff61d9df69572
Tectu <joel@unormal.org>
2012-06-14 16:01:26 +0200

    drawing demo cleanup

--- demos/drawing/main.c
+++ demos/drawing/main.c
@@ -2,16 +2,6 @@
 #include "hal.h"
 #include "glcd.h"
 
-static WORKING_AREA(waThread1, 128);
-static void Thread1(void *arg) {
-	(void)arg;
-
-	while (TRUE) {
-		palTogglePad(GPIOD, GPIOD_LED1);
-		chThdSleepMilliseconds(500);
-	}
-}
-
 int main(void) {
 	halInit();
 	chSysInit();
@@ -22,8 +12,6 @@ int main(void) {
 	lcdDrawCircle(150, 150, 10, filled, Green);
 	lcdDrawLine(0, 0, lcdGetWidth(), lcdGetHeight(), Yellow);
 
-	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-	
 	while (TRUE) {
 		
 		chThdSleepMilliseconds(200);	
