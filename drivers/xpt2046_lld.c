688cd9c879bdf533ff6065264a7d747a56644f9b
Tectu <joel@unormal.org>
2012-06-15 10:19:34 +0200

    added driver for xpt2046 touchpad driver

---
+++ drivers/xpt2046_lld.c
@@ -0,0 +1,43 @@
+#include "xpt2046_lld.h"
+
+#ifdef TOUCHPAD_USE_XPT2046
+
+__inline uint16_t lld_readX(void) {
+    uint8_t txbuf[1];
+    uint8_t rxbuf[2];
+    uint16_t x;
+
+    txbuf[0] = 0xd0;
+    SET_CS(0);
+    spiSend(&SPID1, 1, txbuf);
+    spiReceive(&SPID1, 2, rxbuf);
+    SET_CS(1);
+
+    x = rxbuf[0] << 4;
+    x |= rxbuf[1] >> 4;
+    
+    return x;
+}
+
+__inline uint16_t lld_readY(void) {
+    uint8_t txbuf[1];
+    uint8_t rxbuf[2];
+    uint16_t y;
+
+    txbuf[0] = 0x90;
+    SET_CS(0);
+    spiSend(&SPID1, 1, txbuf);
+    spiReceive(&SPID1, 2, rxbuf);
+    SET_CS(1);
+
+    y = rxbuf[0] << 4;
+    y |= rxbuf[1] >> 4;
+
+    return y;
+}
+
+__inline uint16_t lld_readZ(void) {
+	return 0;
+}
+
+#endif
---
+++ drivers/xpt2046_lld.h
@@ -0,0 +1,15 @@
+#ifndef XPT2046_LLD_H
+#define XPT2046_LLD_H
+
+#include "glcdconf.h"
+#include "touchpad.h"
+
+#ifdef TOUCHPAD_USE_XPT2046
+
+uint16_t lld_readX(void);
+uint16_t lld_readY(void);
+uint16_t lld_readZ(void);
+
+#endif
+#endif
+
