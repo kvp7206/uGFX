755e5be0b1444126921580614439717decdeb852
Tectu <joel@unormal.org>
2012-07-10 02:14:58 +0200

    added fastMath files

---
+++ fastMath.h
@@ -0,0 +1,30 @@
+/*
+ * fastMath.h
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Created on: 07.09.2011
+ *      Author: Roland Domke
+ *   Copyright: 2011 Roland Domke
+ */
+
+#ifndef FASTMATH_H_
+#define FASTMATH_H_
+
+char sgn(char x);
+double getCos(unsigned int degree);
+double getSin(unsigned int degree);
+unsigned char max(unsigned char a, unsigned char b);
+unsigned char min(unsigned char a, unsigned char b);
+#endif /* FASTMATH_H_ */
