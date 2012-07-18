24692be747aa4d94e264219d6f18bcc6beec76ff
Tectu <joel@unormal.org>
2012-07-18 03:33:53 +0200

    restructorizing

--- lcd.mk
+++ lcd.mk
@@ -1,15 +1,17 @@
 LCDLIB =	$(CHIBIOS)/ext/lcd
 
 include 	$(LCDLIB)/drivers/drivers.mk
+include 	$(LCDLIB)/gui/gui.mk
 
 LCDSRC = 	$(LCDLIB)/glcd.c \
 			$(LCDLIB)/fonts.c \
 			$(LCDLIB)/touchpad.c \
 			$(LCDLIB)/graph.c \
-			$(LCDLIB)/gui.c \
 			$(LCDLIB)/console.c \
 			$(LCDLIB)/fastMath.c \
-			$(LCD_DRIVERS_SRC)
+			$(LCD_DRIVERS_SRC) \
+			$(LCD_GUI_SRC)
 
 LCDINC = 	$(LCDLIB) \
-			$(LCD_DRIVERS_INC)
+			$(LCD_DRIVERS_INC) \
+			$(LCD_GUI_INC)
