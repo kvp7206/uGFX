6195e6763b597e22175213999cc12a979862b207
Tectu <joel@unormal.org>
2012-08-08 13:29:08 +0200

    whitespaces

--- halext/halext.mk
+++ halext/halext.mk
@@ -1,8 +1,9 @@
 # List of all the ChibiOS/RT META files, there is no need to remove the files
 # from this list, you can disable parts of the kernel by editing halconf.h.
-HALSRC += ${CHIBIOS}/os/halext/src/gdisp.c \
+
+HALSRC +=	${CHIBIOS}/os/halext/src/gdisp.c \
 			${CHIBIOS}/os/halext/src/gdisp_fonts.c \
 			${CHIBIOS}/os/halext/src/gdisp_emulation.c
 
 # Required include directories
-HALINC += ${CHIBIOS}/os/halext/include
+HALINC +=	${CHIBIOS}/os/halext/include
