292ca0d44edfcff960e52c0c7d8f5a8578e74068
Tectu <joel@unormal.org>
2012-08-08 22:51:23 +0200

    makefiles update

--- halext/halext.mk
+++ halext/halext.mk
@@ -1,9 +1,9 @@
 # List of all the ChibiOS/RT META files, there is no need to remove the files
 # from this list, you can disable parts of the kernel by editing halconf.h.
 
-HALSRC +=	${CHIBIOS}/os/halext/src/gdisp.c \
-			${CHIBIOS}/os/halext/src/gdisp_fonts.c \
-			${CHIBIOS}/os/halext/src/gdisp_emulation.c
+HALSRC +=	$(LCDLIB)/halext/src/gdisp.c \
+			$(LCDLIB)/halext/src/gdisp_fonts.c \
+			$(LCDLIB)/halext/src/gdisp_emulation.c
 
 # Required include directories
-HALINC +=	${CHIBIOS}/os/halext/include
+HALINC +=	$(LCDLIB)/halext/include
