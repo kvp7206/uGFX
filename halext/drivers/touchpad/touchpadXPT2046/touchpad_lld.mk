22d66771f2a9d85eaf96e5ae2fba4b4ac0f3bce3
Tectu <joel@unormal.org>
2012-08-08 23:28:10 +0200

    added touchpad driver structure for halext

---
+++ halext/drivers/touchpad/touchpadXPT2046/touchpad_lld.mk
@@ -0,0 +1,6 @@
+# List the required driver.
+HLSRC += $(LCDLIB)/halext/drivers/touchpad/touchpadXPT2046/touchpad_lld.c
+
+# Required include directories
+HALINC += $(LCDLIB)/halext/drivers/touchpad/touchpadXPT2046
+
