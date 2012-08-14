72d994626f750a86a7a92b2c7ef87a3cceededb3
Tectu <joel@unormal.org>
2012-08-14 02:12:59 +0200

    removed touchpad prefix on touchpad drivers

---
+++ drivers/touchpad/ADS7843/touchpad_lld.mk
@@ -0,0 +1,6 @@
+# List the required driver.
+LCDSRC += $(LCDLIB)/drivers/touchpad/ADS7843/touchpad_lld.c
+
+# Required include directories
+LCDINC += $(LCDLIB)/drivers/touchpad/ADS7843
+
--- drivers/touchpad/touchpadXPT2046/touchpad_lld.mk
+++
@@ -1,6 +0,0 @@
-# List the required driver.
-LCDSRC += $(LCDLIB)/drivers/touchpad/touchpadXPT2046/touchpad_lld.c
-
-# Required include directories
-LCDINC += $(LCDLIB)/drivers/touchpad/touchpadXPT2046
-
