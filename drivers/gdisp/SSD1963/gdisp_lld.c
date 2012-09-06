8cbb7460651add61895e7b4fc5c989f4d9bfcce2
mobyfab <mobyfab@free.fr>
2012-09-06 23:11:03 +0200

    Increased FSMC delays. They were too low.

--- drivers/gdisp/SSD1963/gdisp_lld.c
+++ drivers/gdisp/SSD1963/gdisp_lld.c
@@ -320,7 +320,7 @@ bool_t GDISP_LLD(init)(void) {
 	GDISP_LLD(writeindex)(SSD1963_SET_DISPLAY_ON);
 #if defined(LCD_USE_FSMC)
 	/* FSMC delay reduced as the controller now runs at full speed */
-	FSMC_Bank1->BTCR[FSMC_Bank+1] = FSMC_BTR1_ADDSET_0 | FSMC_BTR1_DATAST_1 | FSMC_BTR1_BUSTURN_0 ;
+	FSMC_Bank1->BTCR[FSMC_Bank+1] = FSMC_BTR1_ADDSET_0 | FSMC_BTR1_DATAST_2 | FSMC_BTR1_BUSTURN_0 ;
 	FSMC_Bank1->BTCR[FSMC_Bank] = FSMC_BCR1_MWID_0 | FSMC_BCR1_WREN | FSMC_BCR1_MBKEN;
 #endif
 
