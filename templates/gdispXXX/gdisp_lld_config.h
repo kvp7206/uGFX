3c5c7361837c6a27257355312bde48cbfe090fb8
Joel Bodenmann <joel@unormal.org>
2012-11-03 02:23:58 +0100

    docs

---
+++ templates/gdispXXX/gdisp_lld_config.h
@@ -0,0 +1,70 @@
+/*
+    ChibiOS/GFX - Copyright (C) 2012
+                 Joel Bodenmann aka Tectu <joel@unormal.org>
+
+    This file is part of ChibiOS/GFX.
+
+    ChibiOS/GFX is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/GFX is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file    templates/gdispXXX/gdisp_lld_config.h
+ * @brief   GDISP Graphic Driver subsystem low level driver header template.
+ *
+ * @addtogroup GDISP
+ * @{
+ */
+
+#ifndef _GDISP_LLD_CONFIG_H
+#define _GDISP_LLD_CONFIG_H
+
+#if GFX_USE_GDISP || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver hardware support.                                                  */
+/*===========================================================================*/
+
+#define GDISP_DRIVER_NAME				"YourDriverName"
+#define GDISP_LLD(x)					gdisp_lld_##x##_YourDriverName
+
+#define GDISP_HARDWARE_LINES			FALSE
+#define GDISP_HARDWARE_CLEARS			FALSE
+#define GDISP_HARDWARE_FILLS			FALSE
+#define GDISP_HARDWARE_BITFILLS			FALSE
+#define GDISP_HARDWARE_CIRCLES			FALSE
+#define GDISP_HARDWARE_CIRCLEFILLS		FALSE
+#define GDISP_HARDWARE_ELLIPSES			FALSE
+#define GDISP_HARDWARE_ELLIPSEFILLS		FALSE
+#define GDISP_HARDWARE_ARCS				FALSE
+#define GDISP_HARDWARE_ARCFILLS			FALSE
+#define GDISP_HARDWARE_TEXT				FALSE
+#define GDISP_HARDWARE_TEXTFILLS		FALSE
+#define GDISP_HARDWARE_SCROLL			FALSE
+#define GDISP_HARDWARE_PIXELREAD		FALSE
+#define GDISP_HARDWARE_CONTROL			FALSE
+#define GDISP_HARDWARE_QUERY			FALSE
+#define GDISP_HARDWARE_CLIP				FALSE
+
+#define GDISP_SOFTWARE_TEXTFILLDRAW		FALSE
+#define GDISP_SOFTWARE_TEXTBLITCOLUMN	FALSE
+
+#define GDISP_PIXELFORMAT				GDISP_PIXELFORMAT_RGB565
+#define GDISP_PACKED_PIXELS				FALSE
+#define GDISP_PACKED_LINES				FALSE
+
+#endif	/* GFX_USE_GDISP */
+
+#endif	/* _GDISP_LLD_CONFIG_H */
+/** @} */
+
---
+++ templates/touchpadXXX/touchpad_lld.mk
@@ -0,0 +1,6 @@
+# List the required driver.
+GFXSRC += $(GFXLIB)/drivers/touchpad/touchpadYOURDEVICE/touchpad_lld.c
+
+# Required include directories
+GFXINC += $(GFXLIB)/drivers/touchpad/touchpadYOURDEVICE
+
