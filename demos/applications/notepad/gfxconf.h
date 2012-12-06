a88e0903ec9c54a36bfe94e74691c7a063277f0b
Andrew Hannam <andrewh@inmarket.com.au>
2012-12-07 01:05:58 +1000

    Simplify demo gfxconf.h files

--- demos/applications/notepad/gfxconf.h
+++ demos/applications/notepad/gfxconf.h
@@ -31,47 +31,13 @@
 #define GDISP_NEED_MSGAPI		FALSE
 
 /* Builtin Fonts */
-#define GDISP_OLD_FONT_DEFINITIONS		FALSE
 #define GDISP_INCLUDE_FONT_SMALL		FALSE
 #define GDISP_INCLUDE_FONT_LARGER		FALSE
 #define GDISP_INCLUDE_FONT_UI1			FALSE
 #define GDISP_INCLUDE_FONT_UI2			TRUE
 #define GDISP_INCLUDE_FONT_LARGENUMBERS	TRUE
 
-/* Features for the GWIN sub-system. */
-#define GWIN_NEED_BUTTON		FALSE
-#define GWIN_NEED_CONSOLE		FALSE
-#define GWIN_NEED_GRAPH			FALSE
-
-/* Features for the GEVENT sub-system. */
-#define GEVENT_ASSERT_NO_RESOURCE	FALSE
-
-/* Features for the GTIMER sub-system. */
-/* NONE */
-
 /* Features for the GINPUT sub-system. */
 #define GINPUT_NEED_MOUSE		TRUE
-#define GINPUT_NEED_KEYBOARD	FALSE
-#define GINPUT_NEED_TOGGLE		FALSE
-#define GINPUT_NEED_DIAL		FALSE
-
-/* Optional Parameters for various sub-systems */
-/*
-	#define GDISP_MAX_FONT_HEIGHT			16
-	#define GEVENT_MAXIMUM_SIZE				32
-	#define GEVENT_MAX_SOURCE_LISTENERS		32
-	#define GTIMER_THREAD_WORKAREA_SIZE		512
-*/
-
-/* Optional Low Level Driver Definitions */
-/*
-	#define GDISP_USE_CUSTOM_BOARD		FALSE
-	#define GDISP_SCREEN_WIDTH			320
-	#define GDISP_SCREEN_HEIGHT			240
-	#define GDISP_USE_FSMC
-	#define GDISP_USE_GPIO
-	#define GDISP_VMT_NAME1(x)			x##YourDriver1
-	#define GDISP_VMT_NAME2(x)			x##YourDriver2
-*/
 
 #endif /* _GFXCONF_H */
