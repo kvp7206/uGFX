2ed57aea77103e864c25f2478ef7fc43352163f0
Andrew Hannam <andrewh@inmarket.com.au>
2013-02-18 17:31:49 +1000

    TDISP to take ROWS and COLUMNS from board file
    TDISP to take ROWS and COLUMNS from board file

--- drivers/tdisp/HD44780/tdisp_lld_board_example.h
+++ drivers/tdisp/HD44780/tdisp_lld_board_example.h
@@ -29,6 +29,19 @@
 #ifndef _TDISP_LLD_BOARD_H
 #define _TDISP_LLD_BOARD_H
 
+/**
+ * The board may override the default display size.
+ * Uncomment the below if your board needs a non-standard size.
+ */
+/*
+#ifndef TDISP_COLUMNS
+	#define TDISP_COLUMNS		16
+#endif
+#ifndef TDISP_ROWS
+	#define TDISP_ROWS			2
+#endif
+*/
+
 static void init_board(void) {
 	/* Code here */
 	#error "tdispHD44780: You must supply a definition for init_board for your board"
