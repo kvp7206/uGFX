73c5ef26f81cb28cbe2fc29e885ea4eea701de1b
Joel Bodenmann <joel@unormal.org>
2013-07-21 22:20:37 +0200

    fixed license headers

--- drivers/gdisp/Nokia6610GE8/GE8.h
+++ drivers/gdisp/Nokia6610GE8/GE8.h
@@ -2,7 +2,7 @@
  * This file is subject to the terms of the GFX License. If a copy of
  * the license was not distributed with this file, you can obtain one at:
  *
- *              http://chibios-gfx.com/license.html
+ *              http://ugfx.org/license.html
  */
 
 #ifndef GE8_H
